8ae8639a8322ea8fb0b3e2128e81c132137b421d
gjanden <gjanden@symgears.com>
2018-03-20 21:50:04 -0600

    refactored redundancy and prepare for production push

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def index
     @articles = Article.all
@@ -24,31 +25,29 @@ class ArticlesController < ApplicationController
   end
 
   def update
-
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render :edit
     end
-
   end
 
   def destroy
-
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "article was successfully removed"
     redirect_to articles_path
-
   end
 
   def show
-    @article = Article.find(params[:id])
+
   end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title,:description)
     end
